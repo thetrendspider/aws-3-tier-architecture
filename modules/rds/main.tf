27978029c72fda118f918ee591f38956cf372206
thetrendspider <88156501+thetrendspider@users.noreply.github.com>
2023-08-02 09:33:35 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -65,6 +65,12 @@ resource "aws_security_group" "rds_sg" {
       var.ec2_security_group_id
     ]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
